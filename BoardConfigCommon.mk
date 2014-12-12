LineageOS/android_device_lge_g3-common

sepolicy: add device specific-sepolicies
Change-Id: I9f4c68e63a7926781800884853ab3d1b882797f2

BoardConfigCommon.mk
@@ -116,5 +116,13 @@ TARGET_USERIMAGES_USE_EXT4 := true
 # Radio
 BOARD_RIL_CLASS := ../../../device/lge/g3-common/ril/
 
+# SELinux
+BOARD_SEPOLICY_DIRS += device/lge/g3-common/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    qrngd.te \
+    sysinit.te
+
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true

AICP/device_lge_g3-common

BoardConfigCommon.mk
@@ -122,5 +122,38 @@ TARGET_USERIMAGES_USE_EXT4 := true
 # Radio
 BOARD_RIL_CLASS := ../../../device/lge/g3-common/ril/
 
+# SELinux
+include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += device/lge/g3-common/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    device.te \
+    file.te \
+    file_contexts \
+    init_shell.te \
+    genfs_contexts \
+    kcal_dev.te \
+    kernel.te \
+    lge_touch_sysfs.te \
+    mm-pp-daemon.te \
+    mm-qcamerad.te \
+    mpdecision.te \
+    nfc.te \
+    platform_app.te \
+    property.te \
+    property_contexts \
+    rmt_storage.te \
+    sensors.te \
+    servicemanager.te \
+    sysinit.te \
+    system_app.te \
+    tee.te \
+    thermal-engine.te \
+    time_data_file.te \
+    ueventd.te \
+    vold.te \
+    wcnss_service.te \
+    wpa.te
+
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true