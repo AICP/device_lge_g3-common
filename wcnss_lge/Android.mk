LineageOS/android_device_lge_g3-common

g3: Add f400 target build
Change-Id: Ie6d5b08297e967a55822eb18d85e68fc3240677f
Signed-off-by: Byun Jae Myeong <byon0716@gmail.com>

wcnss_lge/Android.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifneq ($(filter d850 d852 d855 vs985,$(TARGET_DEVICE)),)
+ifneq ($(filter d850 d852 d855 vs985 f400s,$(TARGET_DEVICE)),)
 
 LOCAL_PATH:= $(call my-dir)
 

AICP/device_lge_g3-common

(cherry picked from commit 2e51154e9cabe5273a75936f809af0c7f9817de8)

wcnss_lge/Android.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifneq ($(filter d850 d852 d855 vs985,$(TARGET_DEVICE)),)
+ifneq ($(filter d850 d852 d855 vs985 f400,$(TARGET_DEVICE)),)
 
 LOCAL_PATH:= $(call my-dir)
 