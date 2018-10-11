ec46c12a85949a21ae99c14dd77bdc94fd014715
Michele Bono <bono.michele94@gmail.com>
2018-10-11 14:46:37 +0300

    g3-common: Ignore Selinux Neverallows
    Change-Id: I585ae6bac9d0ed303422f8dfa85ecf83932bb2cd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -138,6 +138,7 @@ TARGET_USE_SDCLANG := true
 # SELinux
 include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/lge/g3-common/sepolicy
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
